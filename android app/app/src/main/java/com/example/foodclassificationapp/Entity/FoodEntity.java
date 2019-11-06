71a0b7703c410722846c83c76f26d9e462b837ce
Tuanna2208 <tuanna2208@gmail.com>
2019-11-06 18:37:47 +0700

    update app
    update app

---
+++ android app/app/src/main/java/com/example/foodclassificationapp/Entity/FoodEntity.java
@@ -0,0 +1,73 @@
+package com.example.foodclassificationapp.Entity;
+
+public class FoodEntity {
+    private String name;
+    private String image;
+    private int id;
+    private String calories;
+    private String carbs;
+    private String fats;
+    private String proteins;
+
+    public FoodEntity(String name, String image, int id) {
+        this.name = name;
+        this.image = image;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCalories() {
+        return calories;
+    }
+
+    public void setCalories(String calories) {
+        this.calories = calories;
+    }
+
+    public String getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(String carbs) {
+        this.carbs = carbs;
+    }
+
+    public String getFats() {
+        return fats;
+    }
+
+    public void setFats(String fats) {
+        this.fats = fats;
+    }
+
+    public String getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(String proteins) {
+        this.proteins = proteins;
+    }
+}
